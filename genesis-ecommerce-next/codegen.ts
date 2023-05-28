71adc51ba164af54903bce77df970471a4d57d20
paulomuniz <paulo.muniz@rinobox.com.br>
2023-05-28 00:52:11 -0300

    config codegen

---
+++ genesis-ecommerce-next/codegen.ts
@@ -0,0 +1,21 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "http://localhost:8080/v1/graphql",
+  documents: "./src/**/*.graphql",
+  config: {
+    fetcher: "graphql-request",
+  },
+  generates: {
+    "src/generated/graphql.ts": {
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-query",
+      ],
+    },
+  },
+};
+
+export default config;
