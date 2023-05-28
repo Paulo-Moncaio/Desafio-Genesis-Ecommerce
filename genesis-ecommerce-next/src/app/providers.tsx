6759c57ce01bb5149e9de55821a14a2934c8b79f
paulomuniz <paulo.muniz@rinobox.com.br>
2023-05-28 00:52:45 -0300

    atualizando provider

--- genesis-ecommerce-next/src/app/providers.tsx
+++ genesis-ecommerce-next/src/app/providers.tsx
@@ -1,12 +1,19 @@
 "use client";
-
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import CartProvider from "./context/cartContext";
+
+export const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <CacheProvider>
-      <ChakraProvider>{children}</ChakraProvider>
-    </CacheProvider>
+    <QueryClientProvider client={queryClient}>
+      <CartProvider>
+        <CacheProvider>
+          <ChakraProvider>{children}</ChakraProvider>
+        </CacheProvider>
+      </CartProvider>
+    </QueryClientProvider>
   );
 }
